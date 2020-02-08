374fca8d1991e254d480271b45e8b7eb798eac83
stkw0 <me@kropotkin.rocks>
2020-02-08 15:49:26 +0100

    optimize & set makeopts

--- build_amd64.sh
+++ build_amd64.sh
@@ -7,4 +7,4 @@ mount --make-rslave "$DIR/sys"
 mount --rbind /dev "$DIR/dev"
 mount --make-rslave "$DIR/dev"
 mount --bind /var/cache/binpkgs/amd64 "$DIR/var/cache/binpkgs"
-chroot $DIR emerge -quUDN @world
+chroot $DIR emerge -quUDN @world --jobs=2
