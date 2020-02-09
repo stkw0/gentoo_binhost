cc85193d156dbf34d92100c315799d41b78242c9
stkw0 <me@kropotkin.rocks>
2020-02-09 18:16:43 +0100

    changes

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-DIR="amd64"
+DIR="$1"
 mount -t proc /proc "$DIR/proc"
 mount --rbind /sys "$DIR/sys"
 mount --make-rslave "$DIR/sys"
 mount --rbind /dev "$DIR/dev"
 mount --make-rslave "$DIR/dev"
-mount --bind /var/cache/binpkgs/amd64 "$DIR/var/cache/binpkgs"
+mount --bind /var/cache/binpkgs/$1 "$DIR/var/cache/binpkgs"
+chroot $DIR eix-sync
 chroot $DIR emerge -quUDN @world --jobs=2
--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd /root/gentoo_binhosts || exit
-./build_amd64.sh >> /tmp/build_log
+./build.sh amd64 >> /tmp/build_log
+./build.sh amd64-desktop >> /tmp/build_log
+./build.sh amd64-plasma >> /tmp/build_log
+./build.sh amd64-stable >> /tmp/build_log
