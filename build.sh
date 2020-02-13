45cbfafbef5bd0a081f0afd651181e134945fc90
stkw0 <me@kropotkin.rocks>
2020-02-13 20:06:52 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ mount --make-rslave "$DIR/sys"
 mount --rbind /dev "$DIR/dev"
 mount --make-rslave "$DIR/dev"
 mount --bind /var/cache/binpkgs/$1 "$DIR/var/cache/binpkgs"
-chroot $DIR eix-sync
+chroot $DIR eix-sync 1>/dev/null
 chroot $DIR emerge -quUDN @world --jobs=2
+#chroot $DIR emerge -qc
--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /root/gentoo_binhosts || exit
-./build.sh amd64 >> /tmp/build_log
-./build.sh amd64-desktop >> /tmp/build_log
-./build.sh amd64-plasma >> /tmp/build_log
-./build.sh amd64-stable >> /tmp/build_log
+./build.sh amd64
+./build.sh amd64-desktop
+./build.sh amd64-plasma
+./build.sh amd64-stable
