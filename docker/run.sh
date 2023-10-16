b9c65624cb3c0f1ecd77789aad844cee0c6c8a6f
David Roman <davidroman96@gmail.com>
2023-10-16 23:19:14 +0200

    update run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,7 @@
 docker run -v /var/cache/binpkgs/amd64-testing-musl:/var/cache/binpkgs \
 	   -v /var/tmp/ccache:/var/tmp/ccache \
-	   -it amd64-testing-musl 
-
-# chroot $DIR eix-sync 1>/dev/null
-# chroot $DIR emerge -quDN --with-bdeps=y @world --keep-going
-# chroot $DIR haskell-updater
-# chroot $DIR emerge -qc
+	   -i amd64-testing-musl \
+		eix-sync 1> /dev/null; \
+		emerge -quDN --with-bdeps=y @world --keep-going; \
+		haskell-updater; \
+		emerge -qc;
