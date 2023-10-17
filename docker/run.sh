25ef1543180db0453fffaba02c2a409297eb3533
David Roman <davidroman96@gmail.com>
2023-10-17 22:43:02 +0200

    add systemd/plasma binhost

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,12 @@
-docker run -v /var/cache/binpkgs/amd64-testing-musl:/var/cache/binpkgs \
-	   -v /var/tmp/ccache:/var/tmp/ccache \
-	   -i amd64-testing-musl \
-		eix-sync 1> /dev/null; \
-		emerge -quDN --with-bdeps=y @world --keep-going; \
-		haskell-updater; \
-		emerge -qc;
+function run_docker() {
+	docker run -v /var/cache/binpkgs/${1}:/var/cache/binpkgs \
+		   -v /var/tmp/ccache:/var/tmp/ccache \
+		   -it ${1} \
+			eix-sync 1> /dev/null; \
+			emerge -quDN --with-bdeps=y @world --keep-going; \
+			haskell-updater; \
+			emerge -qc;
+}
+
+run_docker amd64-testing-systemd-plasma
+run_docker amd64-testing-musl
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,2 +1,2 @@
-docker build -t amd64-testing-musl .
-
+docker buildx build --pull -f Dockerfile_musl -t amd64-testing-musl .
+docker buildx build --pull -f Dockerfile_systemd_plasma  -t amd64-testing-systemd-plasma .
