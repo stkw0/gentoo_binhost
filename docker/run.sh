20659900b94cc3a05393b640307f88f9d5b900e4
David Roman <droman@ifae.es>
2023-10-19 17:55:40 +0200

    minor things

--- docker/run.sh
+++ docker/run.sh
@@ -2,19 +2,19 @@ function run_docker() {
 	docker run -v /var/cache/binpkgs/${1}:/var/cache/binpkgs \
 		   -v /var/tmp/ccache:/var/tmp/ccache \
 		   --name ${1} \
-		   -it ${1} bash- c "\
+		   -it ${1} bash -c "\
 			eix-sync 1> /dev/null; \
 			emerge -quDN --with-bdeps=y --keep-going --rebuild-if-new-slot=y      \
 				--rebuild-if-new-rev=y --rebuild-if-new-ver=y --rebuild-if-unbuilt=y \
 				--autounmask-continue=y @world; \
 
 			haskell-updater; \
-			emerge -qc;"
+			emerge -qc;" || exit 1
 
 #eclean-dist;
 
 	# Create a new image with the changes of the last container executed
-	docker commit ${1} ${1}
+	docker commit ${1} ${1} || exit 1
 	
 	# Remove container after commit
 	docker rm ${1}
