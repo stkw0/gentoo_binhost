4c0c586323fc8c35ec4eb0f344d45c720ce91dbe
David Roman <droman@ifae.es>
2023-10-19 17:16:26 +0200

    multiple changes

--- docker/run.sh
+++ docker/run.sh
@@ -1,12 +1,32 @@
 function run_docker() {
 	docker run -v /var/cache/binpkgs/${1}:/var/cache/binpkgs \
 		   -v /var/tmp/ccache:/var/tmp/ccache \
+		   --name ${1} \
 		   -it ${1} \
 			eix-sync 1> /dev/null; \
 			emerge -quDN --with-bdeps=y @world --keep-going; \
 			haskell-updater; \
 			emerge -qc;
+
+#eclean-dist;
+
+	# Create a new image with the changes of the last container executed
+	docker commit ${1} ${1}
+
 }
 
+function run_shell() {
+	docker run -v /var/cache/binpkgs/${1}:/var/cache/binpkgs \
+		   -v /var/tmp/ccache:/var/tmp/ccache \
+		   --name ${1} \
+		   -it ${1}
+}
+
+
+
+#run_shell amd64-testing-systemd-plasma
+#run_shell amd64-testing-musl
+
 run_docker amd64-testing-systemd-plasma
-run_docker amd64-testing-musl
+run_docker amd64-testing-plasma
+#run_docker amd64-testing-musl
--- docker/setup.sh
+++ docker/setup.sh
@@ -1,2 +1,3 @@
-docker buildx build --pull -f Dockerfile_musl -t amd64-testing-musl .
-docker buildx build --pull -f Dockerfile_systemd_plasma  -t amd64-testing-systemd-plasma .
+docker buildx build --pull --build-arg="TAG_NAME=musl" -t amd64-testing-musl .
+docker buildx build --pull --build-arg="TAG_NAME=systemd" --build-arg="PROFILE_NAME=default/linux/amd64/17.1/desktop/plasma/systemd" -t amd64-testing-systemd-plasma .
+docker buildx build --pull --build-arg="TAG_NAME=desktop" --build-arg="PROFILE_NAME=default/linux/amd64/17.1/desktop/plasma" -t amd64-testing-plasma .
